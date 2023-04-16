e41ad531fc8966796181bb95aadb62895fd65515
Luke Valan <lukeanthonyvalan@gmail.com>
2023-04-16 14:29:18 +0530

    poc

---
+++ public/sketch.js
@@ -0,0 +1,35 @@
+let socket
+
+function setup() {
+  createCanvas(400, 400);
+  socket = io.connect('localhost:3000')
+  socket.on('mouse', newDrawing)
+}
+
+function draw() {
+  //background(220);
+}
+
+function mouseDragged() {
+  push()
+  noStroke()
+  fill('black')
+  let data = {
+    x: mouseX,
+    y: mouseY
+  }
+  console.log('Sending: '+data.x + ', ' + data.y)
+  ellipse(data.x, data.y, 20)
+  pop()
+
+  socket.emit('mouse', data)
+}
+
+function newDrawing(data) {
+  push()
+  noStroke()
+  fill('grey')
+  console.log("Receiving from "+socket.id+" : "+data.x+", "+data.y)
+  ellipse(data.x, data.y, 20)
+  pop()
+}
---
+++ server.js
@@ -0,0 +1,21 @@
+var express = require('express')
+var app = express()
+let server = app.listen(3000)
+
+app.use(express.static('public'))
+console.log('My socket server is running');
+
+let socket = require('socket.io')
+let io = socket(server)
+io.sockets.on('connection', newConnection )
+
+function newConnection (socket) {
+    console.log('new connection: ' + socket.id)
+
+    socket.on('mouse', mouseMsg)
+
+    function mouseMsg(data) {
+        socket.broadcast.emit('mouse', data)
+        console.log(data+" from: "+socket.id)
+    }
+}
